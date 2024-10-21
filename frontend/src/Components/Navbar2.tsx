0f8d9c93d020557ffc29b3d6e26e9f7089c08367
Lesedi <skhosanalesedi888@gmail.com>
2024-10-21 09:39:01 +0200

    nav update

--- frontend/src/Components/Navbar2.tsx
+++ frontend/src/Components/Navbar2.tsx
@@ -47,9 +47,11 @@ export const Navbar2 = (props: Navbar2Props) => {
           <div className="flex items-center gap-4 lg:hidden">
             <div>
               {buttons.map((button, index) => (
-                <Button key={index} className="w-full px-4 py-1 bg-orange-500 border-none" {...button}>
-                  {button.title}
-                </Button>
+                 <Link to={button.url} key={index}>
+                 <Button className="px-6 py-2 bg-orange-500 border-none" {...button}>
+                   {button.title}
+                 </Button>
+               </Link>
               ))}
             </div>
             <button
